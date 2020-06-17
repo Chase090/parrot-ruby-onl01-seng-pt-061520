04f357d6db296c03f4492c69fc050ab74cb8a4d1
Carl Michael Oyam <carloyam21@icloud.com>
2020-06-17 07:38:15 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(parrot_says="Squawk")
+def parrot(parrot_says="Squawk!")
   puts parrot_says
   return parrot_says
 end
